a8ea741d9c844f767ab8855e4c7b5e6906faa377
codeWhizperer <Adegbiteademola1999@gmail.com>
2024-02-15 13:52:51 +0100

    feat: add dark mode

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className='dark'>
+      <body className={`${inter.className} dark:bg-gray-800`} >
         <StarknetProvider>
           {children}
         </StarknetProvider>
