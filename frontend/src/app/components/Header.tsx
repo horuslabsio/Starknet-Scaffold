e67c2cd5079d40ed46ad691162bf04329883d939
Darlington02 <darlingtonnnam@gmail.com>
2024-02-19 09:02:46 +0100

    chore: add addressBar in header

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -233,9 +233,7 @@ const Header = () => {
         <div className="flex">
           {
             address ? (
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300">
-                {address?.slice(0, 5)}...{address?.slice(60, 66)}
-              </button>
+              <AddressBar />
             ) : (
               <button onClick={toggleModal} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300">
                 Connect
