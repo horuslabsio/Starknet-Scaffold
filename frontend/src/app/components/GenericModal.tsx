d656ff9db77665b78fb86b2a2629f80bd0d000e9
Cheelax <thomas.belloc@gmail.com>
2024-02-19 10:13:43 +0100

    add a generic modal + show transaction on click

---
+++ frontend/src/app/components/GenericModal.tsx
@@ -0,0 +1,33 @@
+const GenericModal = ({
+  isOpen,
+  onClose,
+  animate,
+  children,
+}: {
+  isOpen: boolean;
+  onClose: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  animate: boolean;
+  children: React.ReactNode;
+}) => {
+  return (
+    isOpen && (
+      <section
+        onClick={onClose}
+        className={`fixed h-screen w-screen grid justify-center items-center z-[50] backdrop-blur ${
+          !isOpen ? "hidden" : ""
+        }`}
+      >
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className={`bg-[#1c1b1f] rounded-[25px] flex flex-col border-[1px] border-solid border-outline-grey transition-[opacity,transform] duration-500 ease-in-out ${
+            animate ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
+          }`}
+        >
+          {children}
+        </div>
+      </section>
+    )
+  );
+};
+
+export default GenericModal;
--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { useEffect, useState, Dispatch, SetStateAction } from "react";
 import { Connector, useConnect, useAccount } from "@starknet-react/core";
 import { LibraryBig } from "lucide-react";
-import TransactionList from "./TransactionList/TransactionList";
+import TransactionModal from "./TransactionList/TransactionModal";
+import GenericModal from "./GenericModal";
 
 const loader = ({ src }: { src: string }) => {
   return src;
@@ -50,140 +51,132 @@ const Wallet = ({
 };
 
 const ConnectModal = ({
-  setOpenModal,
+  isOpen,
+  onClose,
 }: {
-  setOpenModal: Dispatch<SetStateAction<boolean>>;
+  isOpen: boolean;
+  onClose: () => void;
 }) => {
   const [animate, setAnimate] = useState(false);
-  useEffect(() => {
-    setAnimate(true);
-    return () => {
-      setAnimate(false);
-    };
-  }, []);
 
-  const removeModal = () => {
+  const closeModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
     setAnimate(false);
     setTimeout(() => {
-      setOpenModal(false);
+      onClose();
     }, 400);
   };
 
+  useEffect(() => {
+    if (isOpen) {
+      setAnimate(true);
+    } else {
+      setAnimate(false);
+    }
+  }, [isOpen]);
   const { connectors } = useConnect();
 
   return (
-    <section
-      onClick={(e) => {
-        setOpenModal(false);
-        e.stopPropagation();
-      }}
-      className="fixed h-screen w-screen grid justify-center items-center z-[99] backdrop-blur"
-    >
-      <div
-        className={`bg-[#1c1b1f] rounded-[25px] flex flex-col h-[clamp(600px,40vmax,468px)] w-[50vmax] border-[1px] border-solid border-outline-grey lg:h-[clamp(504px,35vmax,520px)] lg:min-w-[620px] lg:w-[50vmax] transition-[opacity,transform] duration-500 ease-in-out ${
-          animate ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
-        }  `}
-      >
-        <div className="flex p-4 w-full lg:p-0 lg:grid lg:grid-cols-5">
-          <div className="basis-5/6 lg:col-span-2  lg:border-r-[1px] lg:border-solid lg:border-outline-grey lg:py-4 lg:pl-8">
-            <h2 className="text-center lg:text-start font-bold text-white text-[1.125em]">
-              Connect a Wallet
-            </h2>
-          </div>
-          <div className="ml-auto lg:col-span-3 lg:py-4 lg:pr-8">
-            <button
-              onClick={(e) => {
-                removeModal();
-                e.stopPropagation();
-              }}
-              className="w-8 h-8  grid place-content-center rounded-full bg-outline-grey  "
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="currentColor"
-                  d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z"
-                />
-              </svg>
-            </button>
-          </div>
+    <GenericModal isOpen={isOpen} onClose={closeModal} animate={animate}>
+      <div className="flex p-4 w-full lg:p-0 lg:grid lg:grid-cols-5">
+        <div className="basis-5/6 lg:col-span-2  lg:border-r-[1px] lg:border-solid lg:border-outline-grey lg:py-4 lg:pl-8">
+          <h2 className="text-center lg:text-start font-bold text-white text-[1.125em]">
+            Connect a Wallet
+          </h2>
         </div>
-        <div className="flex flex-col flex-1 justify-between lg:grid lg:grid-cols-5 ">
-          <div className="px-8  lg:h-full lg:col-span-2  lg:border-r-[1px] lg:border-solid lg:border-outline-grey">
-            <h4 className="mb-[1rem] text-text-grey">Popular</h4>
-
-            <div className="flex flex-col gap-4">
-              {connectors.map((connector, index) => (
-                <Wallet
-                  key={connector.id || index}
-                  src={connector.icon.light!}
-                  name={connector.name}
-                  connector={connector}
-                  alt="alt"
-                />
-              ))}
-            </div>
-          </div>
-          <div className="p-4 border-t-[.5px] border-solid  border-red h-fit lg:h-full lg:border-none lg:col-span-3 lg:px-8 lg:py-0 lg:flex lg:flex-col">
-            <h2 className="lg:text-center lg:mb-[3rem] lg:text-[1.125em]  font-bold">
-              What is a wallet?
-            </h2>
-            <article className="hidden lg:flex  flex-col gap-8 place-content-center text-[0.875em] justify-self-center self-center ">
-              <div className="grid grid-cols-10 items-center  gap-4">
-                <div className="col-span-2 border-solid border-[2px] border-white rounded-[10px] h-[3rem] w-[3rem]">
-                  <Image
-                    alt="text"
-                    loader={loader}
-                    src={
-                      "https://media.istockphoto.com/id/1084096262/vector/concept-of-mobile-payments-wallet-connected-with-mobile-phone.jpg?s=612x612&w=0&k=20&c=noILf6rTUyxN41JnmeFhUmqQWiCWoXlg0zCLtcrabD4="
-                    }
-                    width={100}
-                    height={100}
-                    className="h-full w-full object-cover rounded-[10px]"
-                  />
-                </div>
-                <div className="col-span-8 flex flex-col gap-2 ">
-                  <h4 className="text-[1.14em] font-bold">
-                    A home for your digital assets
-                  </h4>
-                  <p className="text-text-grey">
-                    Wallets are used to send, receive, store, and display
-                    digital assets like Ethereum and NFTs.
-                  </p>
-                </div>
-              </div>
-              <div className="grid grid-cols-10 items-center  gap-4">
-                <div className="col-span-2 border-solid border-[2px] border-white rounded-[10px] h-[3rem] w-[3rem]">
-                  <Image
-                    alt="text"
-                    loader={loader}
-                    src={
-                      "https://media.licdn.com/dms/image/D4E12AQFyWdLwXcJu3Q/article-cover_image-shrink_720_1280/0/1687854784940?e=2147483647&v=beta&t=nNDH-9XEcVYcb1PAc3S78ndQze0126KPOSZmnmMERNg"
-                    }
-                    width={100}
-                    height={100}
-                    className="h-full w-full object-cover rounded-[10px]"
-                  />
-                </div>
-                <div className="col-span-8 flex flex-col gap-2 ">
-                  <h4 className="text-[1.14em] font-bold">
-                    A new way to sign-in
-                  </h4>
-                  <p className="text-text-grey">
-                    Instead of creating new accounts and passwords on every
-                    website, just connect your wallet.
-                  </p>
-                </div>
-              </div>
-            </article>
-          </div>
+        <div className="ml-auto lg:col-span-3 lg:py-4 lg:pr-8">
+          <button
+            onClick={(e) => {
+              closeModal(e);
+              e.stopPropagation();
+            }}
+            className="w-8 h-8  grid place-content-center rounded-full bg-outline-grey  "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z"
+              />
+            </svg>
+          </button>
         </div>
       </div>
-    </section>
+      <div className="flex flex-col flex-1 justify-between lg:grid lg:grid-cols-5 ">
+        <div className="px-8  lg:h-full lg:col-span-2  lg:border-r-[1px] lg:border-solid lg:border-outline-grey">
+          <h4 className="mb-[1rem] text-text-grey">Popular</h4>
+
+          <div className="flex flex-col gap-4">
+            {connectors.map((connector, index) => (
+              <Wallet
+                key={connector.id || index}
+                src={connector.icon.light!}
+                name={connector.name}
+                connector={connector}
+                alt="alt"
+              />
+            ))}
+          </div>
+        </div>
+        <div className="p-4 border-t-[.5px] border-solid  border-red h-fit lg:h-full lg:border-none lg:col-span-3 lg:px-8 lg:py-0 lg:flex lg:flex-col">
+          <h2 className="lg:text-center lg:mb-[3rem] lg:text-[1.125em]  font-bold">
+            What is a wallet?
+          </h2>
+          <article className="hidden lg:flex  flex-col gap-8 place-content-center text-[0.875em] justify-self-center self-center ">
+            <div className="grid grid-cols-10 items-center  gap-4">
+              <div className="col-span-2 border-solid border-[2px] border-white rounded-[10px] h-[3rem] w-[3rem]">
+                <Image
+                  alt="text"
+                  loader={loader}
+                  src={
+                    "https://media.istockphoto.com/id/1084096262/vector/concept-of-mobile-payments-wallet-connected-with-mobile-phone.jpg?s=612x612&w=0&k=20&c=noILf6rTUyxN41JnmeFhUmqQWiCWoXlg0zCLtcrabD4="
+                  }
+                  width={100}
+                  height={100}
+                  className="h-full w-full object-cover rounded-[10px]"
+                />
+              </div>
+              <div className="col-span-8 flex flex-col gap-2 ">
+                <h4 className="text-[1.14em] font-bold">
+                  A home for your digital assets
+                </h4>
+                <p className="text-text-grey">
+                  Wallets are used to send, receive, store, and display digital
+                  assets like Ethereum and NFTs.
+                </p>
+              </div>
+            </div>
+            <div className="grid grid-cols-10 items-center  gap-4">
+              <div className="col-span-2 border-solid border-[2px] border-white rounded-[10px] h-[3rem] w-[3rem]">
+                <Image
+                  alt="text"
+                  loader={loader}
+                  src={
+                    "https://media.licdn.com/dms/image/D4E12AQFyWdLwXcJu3Q/article-cover_image-shrink_720_1280/0/1687854784940?e=2147483647&v=beta&t=nNDH-9XEcVYcb1PAc3S78ndQze0126KPOSZmnmMERNg"
+                  }
+                  width={100}
+                  height={100}
+                  className="h-full w-full object-cover rounded-[10px]"
+                />
+              </div>
+              <div className="col-span-8 flex flex-col gap-2 ">
+                <h4 className="text-[1.14em] font-bold">
+                  A new way to sign-in
+                </h4>
+                <p className="text-text-grey">
+                  Instead of creating new accounts and passwords on every
+                  website, just connect your wallet.
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </GenericModal>
   );
 };
 
@@ -269,8 +262,11 @@ const Header = () => {
           </button>
         )}
       </header>
-      {openConectModal && <ConnectModal setOpenModal={setOpenConnectModal} />}
-      {isTransactionModalOpen && <TransactionList />}
+      <ConnectModal isOpen={openConectModal} onClose={toggleModal} />
+      <TransactionModal
+        isOpen={isTransactionModalOpen}
+        onClose={handleCloseTransactionListClick}
+      />
     </>
   );
 };
