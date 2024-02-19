989adecbeb688a20d8e37302b0320f8e84e205be
Maxime Servais <maximeservais@hotmail.com>
2024-02-19 11:03:38 +0100

    merged conflicts

---
+++ frontend/src/app/components/NetworkSwitcher.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { useNetwork } from "@starknet-react/core";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+
+const NETWORK_MAPPING: { [key: string]: string } = {
+  mainnet: "SN_MAIN",
+  goerli: "SN_GOERLI",
+  // sepolia: "SN_SEPOLIA",
+};
+
+const networks = [
+  {
+    value: "SN_MAIN",
+    label: "Mainnet",
+  },
+  {
+    value: "SN_GOERLI",
+    label: "Goerli",
+  },
+  // {
+  //   value: "SN_SEPOLIA",
+  //   label: "Sepolia",
+  // },
+];
+
+export function NetworkSwitcher() {
+  const { chain } = useNetwork();
+  const [open, setOpen] = React.useState(false);
+  const [selectedNetwork, setSelectedNetwork] = React.useState(
+    NETWORK_MAPPING[chain.network]
+  );
+
+  const switchNetwork = async (newNetworkId: string, networkLabel: string) => {
+    try {
+      await window?.starknet?.request({
+        type: "wallet_switchStarknetChain",
+        params: { chainId: newNetworkId },
+      });
+
+      console.log(`Switched to network ${networkLabel}`);
+      setSelectedNetwork(newNetworkId);
+    } catch (error) {
+      console.error("Failed to switch networks:", error);
+    }
+  };
+
+  // Update selectedNetwork when chain.network changes
+  React.useEffect(() => {
+    setSelectedNetwork(NETWORK_MAPPING[chain.network]);
+  }, [chain.network]);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {selectedNetwork
+            ? networks.find((network) => network.value === selectedNetwork)
+                ?.label
+            : "Select Network..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search network..." />
+          <CommandEmpty>No network found.</CommandEmpty>
+          <CommandGroup>
+            {networks.map((network) => (
+              <CommandItem
+                key={network.value}
+                value={network.value}
+                onSelect={() => {
+                  switchNetwork(network.value, network.label);
+                  setOpen(false);
+                }}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    selectedNetwork === network.value
+                      ? "opacity-100"
+                      : "opacity-0"
+                  )}
+                />
+                {network.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default NetworkSwitcher;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,13 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         "dark-1": "#000",
@@ -20,6 +29,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
+export default config;
