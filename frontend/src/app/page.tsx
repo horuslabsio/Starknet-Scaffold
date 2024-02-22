5cdcf178120d0fbc04cdec23ac7491232418719f
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-02-22 17:50:19 +0100

    Style: Updated layout of main links

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         priority
       />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid md:grid-cols-2 md:text-start text-center  lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left lg:max-w-5xl">
         <a
           href="https://docs.swmansion.com/scarb/docs.html"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
