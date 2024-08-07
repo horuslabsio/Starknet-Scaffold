db0a2d2e3f980904c929a86023307ff5c017f333
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-08-07 17:55:59 +0100

    Fixed modal

--- frontend/src/app/components/ui_components/ConnectModal.tsx
+++ frontend/src/app/components/ui_components/ConnectModal.tsx
@@ -62,92 +62,94 @@ const ConnectModal = () => {
   return (
     <GenericModal
       popoverId="connect-modal"
-      style="text-white border-outline-grey mx-auto flex w-[90vw] flex-col rounded-[25px] border-[1px] border-solid bg-[#1c1b1f] md:h-[30rem] md:w-[45rem]"
+      style="text-white border-outline-grey mx-auto w-[90vw] rounded-[25px] border-[1px] border-solid bg-[#1c1b1f] md:h-[30rem] md:w-[45rem]"
     >
-      <div className="flex p-4 w-full lg:p-0 lg:grid lg:grid-cols-5">
-        <div className="basis-5/6 lg:col-span-2  lg:border-r-[1px] lg:border-solid lg:border-outline-grey lg:py-4 lg:pl-8">
-          <h2 className="text-center my-4 lg:text-start font-bold text-white text-[1.125em]">
-            Connect a Wallet
-          </h2>
-        </div>
-        <div className="ml-auto lg:col-span-3 lg:py-4 lg:pr-8">
-          <button
-            popovertarget="connect-modal"
-            popovertargetaction="hide"
-            className="bg-outline-grey grid h-8 w-8 place-content-center rounded-full"
-          >
-            <Close />
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col flex-1 justify-between lg:grid lg:grid-cols-5 ">
-        <div className="px-8  lg:h-full lg:col-span-2  lg:border-r-[1px] lg:border-solid lg:border-outline-grey">
-          <h4 className="mb-[1rem] text-text-grey font-semibold">Popular</h4>
-
-          <div className="flex flex-col gap-4 py-8">
-            {connectors.map((connector, index) => (
-              <Wallet
-                key={connector.id || index}
-                src={connector.icon.light!}
-                name={connector.name}
-                connector={connector}
-                alt="alt"
-              />
-            ))}
+      <div className="flex flex-col">
+        <div className="flex w-full p-4 lg:grid lg:grid-cols-5 lg:p-0">
+          <div className="lg:border-outline-grey basis-5/6 lg:col-span-2 lg:border-r-[1px] lg:border-solid lg:py-4 lg:pl-8">
+            <h2 className="my-4 text-center text-[1.125em] font-bold text-white lg:text-start">
+              Connect a Wallet
+            </h2>
+          </div>
+          <div className="ml-auto lg:col-span-3 lg:py-4 lg:pr-8">
+            <button
+              popovertarget="connect-modal"
+              popovertargetaction="hide"
+              className="bg-outline-grey grid h-8 w-8 place-content-center rounded-full"
+            >
+              <Close />
+            </button>
           </div>
         </div>
-        <div className="p-4 border-t-[.5px] border-solid  border-red h-fit lg:h-full lg:border-none lg:col-span-3 lg:px-8 lg:py-0 lg:flex lg:flex-col">
-          <h2 className="lg:text-center lg:mb-[3rem] lg:text-[1.125em]  font-bold">
-            What is a wallet?
-          </h2>
-          <article className="hidden lg:flex  flex-col gap-8 place-content-center text-[0.875em] justify-self-center self-center ">
-            <div className="grid grid-cols-10 items-center  gap-4">
-              <div className="col-span-2 border-solid border-[2px] border-white rounded-[10px] h-[3rem] w-[3rem]">
-                <Image
-                  alt="text"
-                  loader={loader}
-                  src={
-                    "https://media.istockphoto.com/id/1084096262/vector/concept-of-mobile-payments-wallet-connected-with-mobile-phone.jpg?s=612x612&w=0&k=20&c=noILf6rTUyxN41JnmeFhUmqQWiCWoXlg0zCLtcrabD4="
-                  }
-                  width={100}
-                  height={100}
-                  className="h-full w-full object-cover rounded-[10px]"
+        <div className="flex flex-1 flex-col justify-between lg:grid lg:grid-cols-5">
+          <div className="lg:border-outline-grey px-8 lg:col-span-2 lg:h-full lg:border-r-[1px] lg:border-solid">
+            <h4 className="text-text-grey mb-[1rem] font-semibold">Popular</h4>
+
+            <div className="flex flex-col gap-4 py-8">
+              {connectors.map((connector, index) => (
+                <Wallet
+                  key={connector.id || index}
+                  src={connector.icon.light!}
+                  name={connector.name}
+                  connector={connector}
+                  alt="alt"
                 />
-              </div>
-              <div className="col-span-8 flex flex-col gap-2 ">
-                <h4 className="text-[1.14em] font-bold">
-                  A home for your digital assets
-                </h4>
-                <p className="text-text-grey">
-                  Wallets are used to send, receive, store, and display digital
-                  assets like Ethereum and NFTs.
-                </p>
-              </div>
+              ))}
             </div>
-            <div className="grid grid-cols-10 items-center  gap-4">
-              <div className="col-span-2 border-solid border-[2px] border-white rounded-[10px] h-[3rem] w-[3rem]">
-                <Image
-                  alt="text"
-                  loader={loader}
-                  src={
-                    "https://media.licdn.com/dms/image/D4E12AQFyWdLwXcJu3Q/article-cover_image-shrink_720_1280/0/1687854784940?e=2147483647&v=beta&t=nNDH-9XEcVYcb1PAc3S78ndQze0126KPOSZmnmMERNg"
-                  }
-                  width={100}
-                  height={100}
-                  className="h-full w-full object-cover rounded-[10px]"
-                />
+          </div>
+          <div className="border-red h-fit border-t-[.5px] border-solid p-4 lg:col-span-3 lg:flex lg:h-full lg:flex-col lg:border-none lg:px-8 lg:py-0">
+            <h2 className="font-bold lg:mb-[3rem] lg:text-center lg:text-[1.125em]">
+              What is a wallet?
+            </h2>
+            <article className="hidden flex-col place-content-center gap-8 self-center justify-self-center text-[0.875em] lg:flex">
+              <div className="grid grid-cols-10 items-center gap-4">
+                <div className="col-span-2 h-[3rem] w-[3rem] rounded-[10px] border-[2px] border-solid border-white">
+                  <Image
+                    alt="text"
+                    loader={loader}
+                    src={
+                      "https://media.istockphoto.com/id/1084096262/vector/concept-of-mobile-payments-wallet-connected-with-mobile-phone.jpg?s=612x612&w=0&k=20&c=noILf6rTUyxN41JnmeFhUmqQWiCWoXlg0zCLtcrabD4="
+                    }
+                    width={100}
+                    height={100}
+                    className="h-full w-full rounded-[10px] object-cover"
+                  />
+                </div>
+                <div className="col-span-8 flex flex-col gap-2">
+                  <h4 className="text-[1.14em] font-bold">
+                    A home for your digital assets
+                  </h4>
+                  <p className="text-text-grey">
+                    Wallets are used to send, receive, store, and display
+                    digital assets like Ethereum and NFTs.
+                  </p>
+                </div>
               </div>
-              <div className="col-span-8 flex flex-col gap-2 ">
-                <h4 className="text-[1.14em] font-bold">
-                  A new way to sign-in
-                </h4>
-                <p className="text-text-grey pb-2">
-                  Instead of creating new accounts and passwords on every
-                  website, just connect your wallet.
-                </p>
+              <div className="grid grid-cols-10 items-center gap-4">
+                <div className="col-span-2 h-[3rem] w-[3rem] rounded-[10px] border-[2px] border-solid border-white">
+                  <Image
+                    alt="text"
+                    loader={loader}
+                    src={
+                      "https://media.licdn.com/dms/image/D4E12AQFyWdLwXcJu3Q/article-cover_image-shrink_720_1280/0/1687854784940?e=2147483647&v=beta&t=nNDH-9XEcVYcb1PAc3S78ndQze0126KPOSZmnmMERNg"
+                    }
+                    width={100}
+                    height={100}
+                    className="h-full w-full rounded-[10px] object-cover"
+                  />
+                </div>
+                <div className="col-span-8 flex flex-col gap-2">
+                  <h4 className="text-[1.14em] font-bold">
+                    A new way to sign-in
+                  </h4>
+                  <p className="text-text-grey pb-2">
+                    Instead of creating new accounts and passwords on every
+                    website, just connect your wallet.
+                  </p>
+                </div>
               </div>
-            </div>
-          </article>
+            </article>
+          </div>
         </div>
       </div>
     </GenericModal>
