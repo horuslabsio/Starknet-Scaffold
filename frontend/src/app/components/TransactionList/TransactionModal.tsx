38acd6e206e9c1908ed49956e70f2e248149605a
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-08-13 10:38:09 +0100

    Updated transactions modal

--- frontend/src/app/components/TransactionList/TransactionModal.tsx
+++ frontend/src/app/components/TransactionList/TransactionModal.tsx
@@ -54,46 +54,44 @@ const TransactionModal = () => {
     <>
       <GenericModal
         popoverId="transaction-modal"
-        style="mt-[5rem] w-full bg-transparent backdrop:mt-[5rem] md:mt-[9rem] md:backdrop:mt-[9rem]"
+        style="mt-[5rem] h-screen w-full bg-transparent backdrop:mt-[5rem] md:mt-[9rem] md:backdrop:mt-[9rem]"
       >
-        <div className="user-modal mx-auto flex h-screen w-full max-w-[--header-max-w] flex-col items-center md:items-end md:px-12">
-          <div className="zoom pt-8">
-            <div className="flex w-[95vw] max-w-[30rem] flex-col gap-4 rounded-[24px] bg-[--background] p-8 shadow-popover-shadow transition-colors duration-500 ease-linear">
-              <div className="mb-8 flex justify-between">
-                <h3 className="text-l text-[--headings]">Transactions</h3>
-                <button
-                  //@ts-ignore
-                  popoverTarget="transaction-modal"
-                  className="text-[--headings]"
-                >
-                  <Close />
-                </button>
-              </div>
-              <div className="flex items-center justify-between">
-                <p className="text-text-primary">Select Network</p>
-                <NetworkSwitcher />
-              </div>
-              <div>
-                <button // @ts-ignore
-                  popoverTarget="transaction-history"
-                  className="flex w-full items-center justify-center gap-2 rounded-[12px] border-[2px] border-solid border-[--headings] p-4 text-[--headings]"
-                >
-                  <span className="text-l">
-                    <Library />
-                  </span>
-                  <span>Transaction History</span>
-                </button>
-              </div>
-              <div>
-                <button
-                  aria-haspopup="dialog"
-                  // @ts-ignore
-                  popoverTarget="add-token-popover"
-                  className="w-full rounded-[12px] bg-accent-secondary p-4 text-background-primary-light"
-                >
-                  Add Token
-                </button>
-              </div>
+        <div className="user-modal mx-auto flex w-full max-w-[--header-max-w] flex-col items-center py-8 md:items-end md:px-12">
+          <div className="zoom flex w-[95vw] max-w-[30rem] flex-col gap-4 rounded-[24px] bg-[--background] p-8 pt-8 shadow-popover-shadow transition-colors duration-500 ease-linear">
+            <div className="mb-8 flex justify-between">
+              <h3 className="text-l text-[--headings]">Transactions</h3>
+              <button
+                //@ts-ignore
+                popoverTarget="transaction-modal"
+                className="text-[--headings]"
+              >
+                <Close />
+              </button>
+            </div>
+            <div className="flex items-center justify-between">
+              <p className="text-text-primary">Select Network</p>
+              <NetworkSwitcher />
+            </div>
+            <div>
+              <button // @ts-ignore
+                popoverTarget="transaction-history"
+                className="flex w-full items-center justify-center gap-2 rounded-[12px] border-[2px] border-solid border-[--headings] p-4 text-[--headings]"
+              >
+                <span className="text-l">
+                  <Library />
+                </span>
+                <span>Transaction History</span>
+              </button>
+            </div>
+            <div>
+              <button
+                aria-haspopup="dialog"
+                // @ts-ignore
+                popoverTarget="add-token-popover"
+                className="w-full rounded-[12px] bg-accent-secondary p-4 text-background-primary-light"
+              >
+                Add Token
+              </button>
             </div>
           </div>
         </div>
--- frontend/src/app/components/header/UserModal.tsx
+++ frontend/src/app/components/header/UserModal.tsx
@@ -10,7 +10,7 @@ import GenericModal from "../ui_components/GenericModal";
 import AccountBalance from "../ui_components/AccountBalance";
 import Close from "svg/Close";
 import useTheme from "../ui_components/hooks/useTheme";
-import ThemeSwitch from "./ThemeSwitch";
+import Copy from "svg/Copy";
 
 const UserModal = () => {
   const { address } = useAccount();
@@ -81,17 +81,7 @@ const UserModal = () => {
                     .concat(address?.slice(-5))}
               </span>
               <span className="rounded-full bg-accent-tertiary p-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M21 8.94a1.31 1.31 0 0 0-.06-.27v-.09a1.07 1.07 0 0 0-.19-.28l-6-6a1.07 1.07 0 0 0-.28-.19a.32.32 0 0 0-.09 0a.88.88 0 0 0-.33-.11H10a3 3 0 0 0-3 3v1H6a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3v-1h1a3 3 0 0 0 3-3V9zm-6-3.53L17.59 8H16a1 1 0 0 1-1-1ZM15 19a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h1v7a3 3 0 0 0 3 3h5Zm4-4a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3v3a3 3 0 0 0 3 3h3Z"
-                  />
-                </svg>
+                <Copy />
               </span>
             </button>
           </div>
