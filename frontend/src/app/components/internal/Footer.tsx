ac67de57f3d1e150c4f2d748d97b1a37fd8648ec
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-08-25 19:56:13 +0100

    Chore update icons

--- frontend/src/app/components/internal/Footer.tsx
+++ frontend/src/app/components/internal/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="flex h-20 flex-col items-center justify-center bg-accent-secondary">
       <div className="p-6">
-        <p className="text-center text-[#BC988C]">Built with ❤️ by Argent & Horus Labs</p>
+        <p className="text-center text-[#BC988C]">Built with ❤️ Horus Labs</p>
       </div>
     </footer>
   );
