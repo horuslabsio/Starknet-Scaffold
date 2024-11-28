d5b947a27555ed0b8a2f12d92cd09e80cfa610db
Oshioke Salaki <salaki1902@gmail.com>
2024-11-28 11:19:07 +0100

    fix: correct hotjar_id

--- frontend/src/app/components/internal/Analytics.tsx
+++ frontend/src/app/components/internal/Analytics.tsx
@@ -10,7 +10,7 @@ const Analytics = () => {
         __html: `
         (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:5222700,hjsv:6};
+            h._hjSettings={hjid:5224041,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
